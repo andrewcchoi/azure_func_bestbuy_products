3e7fc7750fbf73865e4bc9376c7ae5fd37a1f31a
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2021-12-05 20:26:46 -0800

    Add files via upload

---
+++ api_bestbuy_async v2.py
@@ -0,0 +1,35 @@
+import aiohttp
+import asyncio
+import json
+
+
+# async def fetch(session):
+async def main():
+    url = f"https://api.bestbuy.com/v1/products"
+    payload = {
+                "apiKey": "AwGV1zCqy6FDoQHNUoNcfjqA",
+                "pageSize": 100,
+                "format": "json",
+                "show": "all",
+                "page": 1
+            }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=payload) as resp:
+            data = await resp.json()
+        print(resp.status)
+        # print(json.dumps(data, indent=4))
+
+    print(type(json.dumps(data))) # str
+    print(type(data)) # dict
+    return data
+
+    # async with aiohttp.ClientSession() as session:
+    # data = await fetch()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+    # loop.run_until_complete(asyncio.gather(*(get_player(*args) for args in player_args)))
+
---
+++ schema.sql
@@ -0,0 +1,49 @@
+-- DROP TABLE IF EXISTS bestbuy.products;
+DROP TABLE IF EXISTS products;
+
+
+-- sqlserver
+CREATE TABLE products (
+"id" INT IDENTITY(1,1) PRIMARY KEY
+, "nextCursorMark" CHAR(36)
+, "total" INT
+, "totalPages" INT
+, "queryTime" FLOAT
+, "totalTime" FLOAT
+, "canonicalUrl" VARCHAR(256)
+, "sku" CHAR(8)
+, "name" VARCHAR(256)
+, "type" VARCHAR(8)
+, "startDate" DATE
+, "new" BIT
+, "activeUpdateDate" DATETIME2
+, "active" BIT
+, "regularPrice" FLOAT
+, "salePrice" FLOAT
+, "clearance" BIT
+, "onSale" BIT
+, "categoryPath" VARCHAR(512)
+, "customerReviewCount" FLOAT
+, "customerReviewAverage" FLOAT
+, "priceUpdateDate" DATETIME2
+, "itemUpdateDate" DATETIME2
+, "class" VARCHAR(20)
+, "classId" CHAR(3)
+, "subclass" VARCHAR(20)
+, "subclassId" CHAR(4)
+, "department" VARCHAR(20)
+, "departmentId" CHAR(2)
+, "theatricalReleaseDate" INT
+, "studio" VARCHAR(256)
+, "manufacturer" VARCHAR(30)
+, "modelNumber" VARCHAR(30)
+, "condition" VARCHAR(20)
+, "artistName" VARCHAR(256)
+, "images" VARCHAR(MAX)
+, "image" VARCHAR(256)
+, "color" VARCHAR(100)
+, "request_timestamp" DATETIME2 NOT NULL
+, "record_timestamp" DATETIME2 DEFAULT SYSUTCDATETIME() NOT NULL
+);
+
+-- ALTER TABLE products ADD CONSTRAINT DF_products DEFAULT SYSUTCDATETIME() FOR record_timestamp;
