f6ed67e0207b0aeed2fa94cdca6f069a94842626
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2023-03-12 01:45:48 -0800

    add global variable for azure function

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -6,17 +6,18 @@ import azure.functions as func
 
 from TimerTrigger1.api_bestbuy_async_discount_alert import bb_main
 
+LAST_UPDATE_DATE = '2023-03-11T17:53:43'
 
 def main(mytimer: func.TimerRequest) -> None:
-
+    global LAST_UPDATE_DATE
     logging.info('ACTION!'.center(20, "*"))
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(bb_main())
-
+    max_price_date = loop.run_until_complete(bb_main(last_update_date=LAST_UPDATE_DATE))
+    LAST_UPDATE_DATE = max_price_date[0]
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
