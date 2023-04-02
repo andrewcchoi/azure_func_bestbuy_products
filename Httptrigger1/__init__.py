25cff931182b3535ed4a50b52a5da338b0ead4f7
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2023-04-02 00:59:18 -0700

    added a tag href to url.

--- Httptrigger1/__init__.py
+++ Httptrigger1/__init__.py
@@ -42,13 +42,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     df_discount = loop.run_until_complete(bb_main(last_update_date=LAST_UPDATE_DATE))
+    df_discount = df_discount.replace('http', '<a href="http')
+    df_discount = df_discount.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
     
     # * run macbook module
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     df_macbook = loop.run_until_complete(macbook_main())
     df_macbook = df_macbook.replace('http', '<a href="http')
-    df_macbook = df_macbook.replace('/pdp', '/pdp" target="_blank">url</a>')
+    df_macbook = df_macbook.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
     df_macbook = df_macbook.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
     
     
