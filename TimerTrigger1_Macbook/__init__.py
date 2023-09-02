b7ec5b0a1b22393900077557864d54281ef1de95
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2023-09-02 16:22:50 -0700

    refactor to use standard python file.

--- TimerTrigger1_Macbook/__init__.py
+++ TimerTrigger1_Macbook/__init__.py
@@ -2,20 +2,40 @@ import os
 import datetime
 import logging
 import asyncio
+import azure.functions as func
 
+from typing import List
 from dateutil import tz
 from datetime import datetime, timedelta
 
-import azure.functions as func
+from api_bestbuy_async import bb_main
 
-from TimerTrigger1_Macbook.api_bestbuy_async_macbook_alert import bb_main
-from api_bestbuy_async_nvidia_pc_alert import bb_main_nvidia
+def async_call(url: str, email: bool=True) -> None:
+    """Makes an asynchronous call to a given URL.
+
+    :param url: The URL to call.
+    :param email: Whether to send an email notification after the call. Defaults to True.
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(bb_main(url=url, email=email))
+
 
 def main(mytimer: func.TimerRequest) -> None:
+    """Runs a Python timer trigger function.
+
+    This function logs an action message, gets the current date and time in UTC and US/Pacific zones,
+    formats the date and time as a string, calls two URLs asynchronously using the async_call function,
+    and logs the timer status and the UTC timestamp.
+
+    Args:
+        mytimer (func.TimerRequest): A timer request object.
+
+    Raises:
+        Exception: If any error occurs during the execution of the function.
+    """
 
     logging.info('ACTION!'.center(20, "*"))
-    # utc_timestamp = datetime.datetime.utcnow().replace(
-    #     tzinfo=datetime.timezone.utc).isoformat()
 
     from_zone = tz.gettz('UTC')
     to_zone = tz.gettz('US/Pacific')
@@ -25,16 +45,14 @@ def main(mytimer: func.TimerRequest) -> None:
     local = utc_timestamp.astimezone(to_zone) # * convert to local time
 
     LAST_UPDATE_DATE = local.strftime('%Y-%m-%dT%H:%M:%S') # * apply string format to date timestamp
-    
-    # macbook deals
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(bb_main(last_update_date=LAST_UPDATE_DATE))
-    
-    # nvidia pcs
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(bb_main_nvidia(last_update_date=LAST_UPDATE_DATE))
+    urls: List[str] = [
+        "https://api.bestbuy.com/v1/products(department=COMPUTERS&class=APPLE LAPTOP&orderable=Available&onlineAvailability=trueonSale=true&active=true&manufacturer=Apple)", 
+        "https://api.bestbuy.com/v1/products(categoryPath.name=laptop*&orderable=Available&onlineAvailability=true&active=true&onSale=true&details.value=nvidia)"
+        ]
+
+    # macbook and nvidia pcs deals
+    for url in urls:
+        async_call(url=url)
     
     if mytimer.past_due:
         logging.info('The timer is past due!')
