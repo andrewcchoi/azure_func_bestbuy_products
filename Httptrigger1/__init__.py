892b7bfe095e26b186af9818f02aff728d90897c
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2023-09-02 19:16:58 -0700

    add docstring, call fx, and remove repeated code.

--- Httptrigger1/__init__.py
+++ Httptrigger1/__init__.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 import asyncio
 
+from typing import Tuple, Dict
 from dateutil import tz
 from datetime import datetime, timedelta
 
@@ -11,6 +12,38 @@ import azure.functions as func
 from Httptrigger1.api_bestbuy_async_discount_alert import bb_main
 from Httptrigger1.api_bestbuy_async_macbook_alert import bb_main as macbook_main
 from Httptrigger1.api_bestbuy_async_nvidia_pc_alert import bb_main_nvidia
+
+def async_call(url: str, subject: str, email: bool=False)  -> Tuple[str, Tuple]:
+    """Makes an asynchronous call to a given URL.
+
+    :param url: The URL to call.
+    :type url: str
+    :param subject: The subject of the email notification.
+    :type subject: str
+    :param email: Whether to send an email notification after the call. Defaults to False.
+    :type email: bool
+    :return: A tuple of the modified dataframe and its shape.
+    :rtype: tuple(str, tuple)
+
+    .. code-block:: python
+
+        # Example of using async_call function
+        url = "https://api.bestbuy.com/v1/products(longDescription=iPhone*|sku=7619002)"
+        subject = "Best Buy Deals"
+        email = False
+        df, df_shape = async_call(url, subject, email)
+        print(df)
+        print(df_shape)
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    df_html, df_shape = loop.run_until_complete(bb_main(url=url, subject=subject, email=email))
+    df_html = df_html.replace('http', '<a href="http')
+    df_html = df_html.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
+    df_html = df_html.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
+
+    return df_html, df_shape
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
@@ -39,49 +72,38 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     LAST_UPDATE_DATE = local.strftime('%Y-%m-%dT%H:%M:%S') # * apply string format to date timestamp
     
-    # * run discount module
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    df_discount = loop.run_until_complete(bb_main(last_update_date=LAST_UPDATE_DATE))
-    df_discount = df_discount.replace('http', '<a href="http')
-    df_discount = df_discount.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
-    df_discount = df_discount.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
-    
-    # * run macbook module
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    df_macbook, df_macbook_shape = loop.run_until_complete(macbook_main())
-    df_macbook = df_macbook.replace('http', '<a href="http')
-    df_macbook = df_macbook.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
-    df_macbook = df_macbook.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
-    
-    # * run nvidia laptop module
-    url = f"https://api.bestbuy.com/v1/products(categoryPath.name=laptop*&onSale=true&orderable=Available&onlineAvailability=true&active=true&details.value=nvidia&details.value!=1650*&details.value!=1660*)"
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    df_nvidia_laptop, df_nvidia_laptop_shape = loop.run_until_complete(bb_main_nvidia(url=url))
-    df_nvidia_laptop = df_nvidia_laptop.replace('http', '<a href="http')
-    df_nvidia_laptop = df_nvidia_laptop.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
-    df_nvidia_laptop = df_nvidia_laptop.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
-    
-    # * run nvidia desktop module
-    url = f"https://api.bestbuy.com/v1/products(categoryPath.name=desktop*&onSale=true&orderable=Available&onlineAvailability=true&active=true&details.value=nvidia&details.value!=1650*&details.value!=1660*)"
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    df_nvidia_desktop, df_nvidia_desktop_shape = loop.run_until_complete(bb_main_nvidia(url=url))
-    df_nvidia_desktop = df_nvidia_desktop.replace('http', '<a href="http')
-    df_nvidia_desktop = df_nvidia_desktop.replace('/pdp', '/pdp" target="_blank">urlLink</a>')
-    df_nvidia_desktop = df_nvidia_desktop.replace('/cart', '/cart" target="_blank">addToCartUrl</a>')
-    
-    
+    # Define a type alias for the inner dictionary
+    inner_query = Dict[str, str]
+
+    # dictionary with url queries and email subject
+    queries: Dict[str, inner_query] = {
+        "discounts": {
+            "url":f"https://api.bestbuy.com/v1/products(priceUpdateDate>{LAST_UPDATE_DATE}&onSale=true&active=true&percentSavings>50&salePrice<>60696.99)",
+            "subject": f'HttpTrigger1_Discounts - Best Buy Deals ({datetime.now()})'
+        },
+        "macbooks": {
+            "url":"https://api.bestbuy.com/v1/products(categoryPath.name=macbook*&details.value!=intel*&orderable=Available&onlineAvailability=true&onSale=true&active=true)",
+            "subject": f'HttpTrigger1_Macbooks - Best Buy Deals ({datetime.now()})'
+        },
+        "laptop": { # details.name=Advanced Graphics Rendering Technique*
+            "url":"https://api.bestbuy.com/v1/products(categoryPath.name=laptop*&onSale=true&orderable=Available&onlineAvailability=true&active=true&details.value=nvidia&details.value!=1650*&details.value!=1660*)",
+            "subject": f'HttpTrigger1_Nvidia_Laptops - Best Buy Deals ({datetime.now()})'
+        },
+        "desktop": {
+            "url":"https://api.bestbuy.com/v1/products(categoryPath.name=desktop*&onSale=true&orderable=Available&onlineAvailability=true&active=true&details.value=nvidia&details.value!=1650*&details.value!=1660*)",
+            "subject": f'HttpTrigger1_Nvidia_Desktops - Best Buy Deals ({datetime.now()})'
+        }
+    }
+
+    http_response: str = f"Hello {name}!</br>"
+
+    # macbook and nvidia pcs deals
+    for query in queries.keys():
+        html, shape = async_call(url=queries[query]["url"], subject=queries[query]["subject"], email=False)
+        http_response += f"{query} {shape}:</br>{html}</br>"
+
     if name:
-        return func.HttpResponse(f"""
-                                 Hello {name}!</br>
-                                 Discounts:</br>{df_discount}</br>
-                                 Macbook Discounts {df_macbook_shape}:</br>{df_macbook}</br>
-                                 NVIDIA Laptops {df_nvidia_laptop_shape}:</br>{df_nvidia_laptop}</br>
-                                 NVIDIA Desktops {df_nvidia_desktop_shape}:</br>{df_nvidia_desktop}"""
-                                 , mimetype="text/html")
+        return func.HttpResponse(http_response, mimetype="text/html")
     
     else:
         return func.HttpResponse(
