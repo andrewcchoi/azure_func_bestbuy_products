8e4db9acc1ac555ae740563b469fa7c622f30b77
andrew choi <74335726+andrewcchoi@users.noreply.github.com>
2024-02-03 07:48:40 -0800

    changed filter parameters.

--- TimerTrigger1_Macbook/__init__.py
+++ TimerTrigger1_Macbook/__init__.py
@@ -121,7 +121,7 @@ def main(mytimer: func.TimerRequest) -> None:
             "offers":[]
         },
         "television": {
-            "url":"https://api.bestbuy.com/v1/products(productTemplate in(Televisions,Digital_Signage_Displays_and_Players,Portable_TVs_and_Video)&screenSizeClassIn>39&salePrice<200&onSale=true&orderable=Available&onlineAvailability=true&active=true)",
+            "url":"https://api.bestbuy.com/v1/products(productTemplate in(Televisions,Digital_Signage_Displays_and_Players,Portable_TVs_and_Video)&screenSizeClassIn>39&salePrice<999&details.value!=Full HD&onSale=true&orderable=Available&onlineAvailability=true&active=true)",
             "subject": f'TimerTrigger1_Television - Best Buy Deals ({datetime.now()})',
             "columns":None,
             "detail_names":["Built-In Speakers", "Resolution", "Display Type", "Screen Size Class", "Screen Size", "Curved Screen"],
